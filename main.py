f7891f6d40990ce3c747c8fd19a1ac1df6e159b9
flex <geroi_cool@mail.ru>
2022-01-24 19:36:24 +0300

    1 задание, 2 - пытаюсь разобраться с API вк, пока разбираюсь с токеном

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-#
+import requests
+import json
+
+
+url = 'https://api.github.com'
+user = 'Novopashin-GS'
+response = requests.get(f'{url}/users/{user}/repos').json()
+with open('data.json', 'w', encoding='UTF-8') as data:
+    json.dump(response, data)
+
+
